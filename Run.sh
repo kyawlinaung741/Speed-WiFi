8e1b1fc652d97da68afde0f8852add60d9246e11
kyawlinaung741 <55587156+kyawlinaung741@users.noreply.github.com>
2021-12-20 11:39:40 +0800

    Create Run.sh

---
+++ Run.sh
@@ -0,0 +1,49 @@
+clear
+cyan='\033[0;36m'
+purple='\033[0;35m'
+brown='\033[0;33m'
+lightgray='\033[0;37m'
+darkgray='\033[1;30m'
+lightblue='\033[1;34m'
+lightgreen='\033[1;32m'
+lightcyan='\033[1;36m'
+lightred='\033[1;31m'
+lightpurple='\033[1;35m'
+yellow='\033[1;33m'
+white='\033[1;37m'
+sleep 1
+echo
+figlet "Speed"  | lolcat
+toilet -f standard "Wifi" -F gay
+date | lolcat
+echo ______________________________________√
+echo $lightpurple "speed wifi" | lolcat
+echo ________________________________¶
+sleep 2
+echo $yellow"Select the tools :"
+echo
+echo $lightgray"1.)Stabilize wifi Network "
+echo
+echo $darkgray"2.)Exit"
+echo
+echo $lightred
+read -p "choose menu : " bro
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+clear
+figlet "Speed wifi" | lolcat
+echo $white"speed wifi"
+echo ®____________Running Speed_____________ √
+sleep 2
+ping -s10000 8.8.8.8,8.8.4.4
+fi
+if [ $bro = 2 ] || [ $bro = 2 ]
+then
+clear
+echo $yellow
+figlet "speed wifi"
+echo "speed wifi"
+echo
+sleep 3
+exit
+fi
